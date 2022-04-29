9d2d3b5e2f1e769532ec649925f95deeb20b5e60
Jastoni <47589812+vjastoni@users.noreply.github.com>
2022-04-29 11:17:28 +0800

    adds tailwind utilities

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import App from './components/App'
+import NavigationBar from './components/NavigationBar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <NavigationBar/>
     <App />
   </React.StrictMode>
 )
