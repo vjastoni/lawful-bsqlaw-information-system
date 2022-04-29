98152d2f7ca193c77b085b3fa626345ab4685948
Jastoni <47589812+vjastoni@users.noreply.github.com>
2022-04-29 11:40:03 +0800

    adds preliminary navigation styles

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 function NavigationBar() {
-  return <div>NavigationBar</div>
+  return <div className='bg-red-800 p-6 flex justify-between'>
+    <div className=''>
+      BSQ Logo
+    </div>
+    <div className="flex justify-around padding-2 text-gray-200">
+      <li className='list-none'><a href="#">Home</a></li>
+      <li className='list-none'><a href="#">About</a></li>
+      <li className='list-none'><a href="#">Contact Us</a></li>
+      <li className='list-none'><a href="#">Get Started</a></li>
+    </div>
+  </div>
 }
 
 export default NavigationBar
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App'
 import NavigationBar from './components/NavigationBar'
+import './styles/style.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
