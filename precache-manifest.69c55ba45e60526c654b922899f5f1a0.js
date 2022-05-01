157ecc836a4c837ff79606caba2e84b3f9890b37
rodiemertomi <rodiemertominez@gmail.com>
2022-05-01 15:27:04 +0800

    Updates

---
+++ precache-manifest.69c55ba45e60526c654b922899f5f1a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f333849709f0a10a36d",
+    "url": "/rodiemertomi.github.io/bsqlawful/static/css/main.ff7f638d.chunk.css"
+  },
+  {
+    "revision": "6f333849709f0a10a36d",
+    "url": "/rodiemertomi.github.io/bsqlawful/static/js/main.6f333849.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/rodiemertomi.github.io/bsqlawful/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "2ded161d425ce65a7129",
+    "url": "/rodiemertomi.github.io/bsqlawful/static/js/2.2ded161d.chunk.js"
+  },
+  {
+    "revision": "4b5de9edf5c537f87bc5",
+    "url": "/rodiemertomi.github.io/bsqlawful/static/js/runtime~main.4b5de9ed.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/rodiemertomi.github.io/bsqlawful/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2645a41f481b2b66610f10b79d31259e",
+    "url": "/rodiemertomi.github.io/bsqlawful/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/rodiemertomi.github.io/bsqlawful/precache-manifest.69c55ba45e60526c654b922899f5f1a0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/rodiemertomi.github.io/bsqlawful/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
