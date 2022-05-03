6a923548929089b152b720395bf8483f4cff6cb8
Jastoni <47589812+vjastoni@users.noreply.github.com>
2022-05-04 00:04:09 +0800

    adds styling for medium breakpoint

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Button = props => {
+const Button = (props) => {
   return (
     <div>
-      <button className='font-bold py-4 px-8 rounded-3xl border-white border-2 bg-gray hover:text-yellow hover:bg-maroon text-black'>
+      <button className="font-bold py-4 px-8 rounded-3xl border-white border-2 bg-gray hover:text-yellow hover:bg-maroon text-black md:text-sm md:py-3 md:px-4">
         {props.children}
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
